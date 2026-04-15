d495f975274904dc5775d3f7025d3c93899fd135
HolyExcalibur <gamerfiend.jr@gmail.com>
2026-04-15 15:34:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import {defineConfig, loadEnv} from 'vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Vegie365/', // Add this line!
-})
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {

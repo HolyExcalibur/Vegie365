f6ffcd03ba9adb179ebddf9e202424bec402067b
HolyExcalibur <gamerfiend.jr@gmail.com>
2026-04-15 18:36:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     // ADD THIS LINE BELOW:
-    base: '/', 
+    base: '/Vegie365/', 
     
     plugins: [react(), tailwindcss()],
     define: {
